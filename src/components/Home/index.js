20935f911cdbc35e697dd5a20c5badbe08f171e4
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-02-08 23:59:27 +1100

    added details and fixed layout - need to address navbar spacing issue

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,28 +1,26 @@
-import './index.scss';
-import LogoTitle from '../../assets/images/logo-s.png';
 import { Link } from 'react-router-dom';
+import LogoTitle from '../../assets/images/logo-s.png';
+import './index.scss';
 
 const Home = () => {
   return (
-    <>
-      <div className="container home-page">
-        <div className="text-zone">
-          <h1>
-            Hi, <br /> I'm{' '}
-          </h1>
+    <div className="container home-page">
+      <div className="text-zone">
+        <h1>
+          Hi, <br /> I'm
           <img src={LogoTitle} alt="" />
           lobodan
           <br />
           web developer
-          <h2>Jnr. Software Engineer / Science Content Specialist</h2>
-          <Link to="/contact" className="flat-button">
-            {' '}
-            Contact me
-          </Link>
-        </div>
+        </h1>
+        <h2>Jnr. Software Engineer / Science Content Specialist</h2>
+        <Link to="/contact" className="flat-button">
+          Contact me
+        </Link>
       </div>
-    </>
+    </div>
   );
 };
 
 export default Home;
+// 3300
