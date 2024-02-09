f01920d272d460e5c19ef2588cd5327227474c5e
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-02-09 23:02:06 +1100

    implemented animated letters component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,17 +1,37 @@
 import { Link } from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-s.png';
 import './index.scss';
+import { useState } from 'react';
+import AnimatedLetters from '../AnimatedLetters';
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate');
+  const nameString = 'lobodan';
+  const nameArray = nameString.split('');
+  const jobString = 'Web Developer.';
+  const jobArray = jobString.split('');
+
   return (
     <div className="container home-page">
       <div className="text-zone">
         <h1>
-          Hi, <br /> I'm
-          <img src={LogoTitle} alt="" />
-          lobodan
+          <span className={letterClass}>H</span>
+          <span className={`${letterClass} _12`}>i,</span>
           <br />
-          web developer
+          <span className={`${letterClass} _13`}>I</span>
+          <span className={`${letterClass} _14`}>'m</span>
+          <img src={LogoTitle} alt="" />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={nameArray}
+            idx={15}
+          />
+          <br />
+          <AnimatedLetters
+            letterClass={letterClass}
+            strArray={jobArray}
+            idx={22}
+          />
         </h1>
         <h2>Jnr. Software Engineer / Science Content Specialist</h2>
         <Link to="/contact" className="flat-button">
