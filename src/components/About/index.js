8cbe5ec1de29c13fc1bcf7015c736dc825ac2d71
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-02-12 23:41:52 +1100

    removed double up code

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,15 @@
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
 import { useEffect, useState } from 'react';
+import {
+  faAngular,
+  faCss3,
+  faGitAlt,
+  faHtml5,
+  faJsSquare,
+  faReact,
+} from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const About = () => {
   // const aboutMeString = 'About Me';
@@ -31,6 +40,29 @@ const About = () => {
         <p>Example p text 1 </p>
         <p>Example p text 2 </p>
         <p>Example p text 3 </p>
+      </div>
+
+      <div className="stage-cube-cont">
+        <div className="cubespinner">
+          <div className="face1">
+            <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+          </div>
+          <div className="face2">
+            <FontAwesomeIcon icon={faHtml5} color="#DD0031" />
+          </div>
+          <div className="face3">
+            <FontAwesomeIcon icon={faCss3} color="#DD0031" />
+          </div>
+          <div className="face4">
+            <FontAwesomeIcon icon={faReact} color="#DD0031" />
+          </div>
+          <div className="face5">
+            <FontAwesomeIcon icon={faJsSquare} color="#DD0031" />
+          </div>
+          <div className="face6">
+            <FontAwesomeIcon icon={faGitAlt} color="#DD0031" />
+          </div>
+        </div>
       </div>
     </div>
   );
