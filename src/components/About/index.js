b459d207f7be8deccea1c1d7a7692bfea90c6a67
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-02-13 19:36:11 +1100

    added and styled spinny cube

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,7 @@
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
 import { useEffect, useState } from 'react';
+import Loader from 'react-loaders';
 import {
   faAngular,
   faCss3,
@@ -15,7 +16,7 @@ const About = () => {
   // const aboutMeString = 'About Me';
   const aboutMeArray = 'About Me'.split('');
   const [letterClass, setLetterClass] = useState('text-animate');
-  console.log(aboutMeArray);
+  // console.log(aboutMeArray);
 
   useEffect(() => {
     let timeoutId = setTimeout(() => {
@@ -28,43 +29,46 @@ const About = () => {
   }, []);
 
   return (
-    <div className="container about-page">
-      <div className="text-zone">
-        <h1>
-          <AnimatedLetters
-            strArray={aboutMeArray}
-            idx={15}
-            letterClass={letterClass}
-          />
-        </h1>
-        <p>Example p text 1 </p>
-        <p>Example p text 2 </p>
-        <p>Example p text 3 </p>
-      </div>
+    <>
+      <div className="container about-page">
+        <div className="text-zone">
+          <h1>
+            <AnimatedLetters
+              strArray={aboutMeArray}
+              idx={15}
+              letterClass={letterClass}
+            />
+          </h1>
+          <p>Example p text 1 </p>
+          <p>Example p text 2 </p>
+          <p>Example p text 3 </p>
+        </div>
 
-      <div className="stage-cube-cont">
-        <div className="cubespinner">
-          <div className="face1">
-            <FontAwesomeIcon icon={faAngular} color="#DD0031" />
-          </div>
-          <div className="face2">
-            <FontAwesomeIcon icon={faHtml5} color="#DD0031" />
-          </div>
-          <div className="face3">
-            <FontAwesomeIcon icon={faCss3} color="#DD0031" />
-          </div>
-          <div className="face4">
-            <FontAwesomeIcon icon={faReact} color="#DD0031" />
-          </div>
-          <div className="face5">
-            <FontAwesomeIcon icon={faJsSquare} color="#DD0031" />
-          </div>
-          <div className="face6">
-            <FontAwesomeIcon icon={faGitAlt} color="#DD0031" />
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#DD0031" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#DD0031" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#DD0031" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#DD0031" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#DD0031" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+      <Loader type="pacman" />
+    </>
   );
 };
 
