bd46799166ebcd6feb6aaf112d6b3649beb6a358
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-02-14 23:12:16 +1100

    initialising work component

---
+++ src/components/Work/index.js
@@ -0,0 +1,34 @@
+import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
+import { useEffect, useState } from 'react';
+
+const Work = () => {
+  const portfolioArray = 'Work'.split('');
+  const [letterClass, setLetterClass] = useState('text-animate');
+
+  useEffect(() => {
+    let timeoutId = setTimeout(() => {
+      setLetterClass('text-animate-hover');
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="container portfolio-page">
+        <h1>
+          <AnimatedLetters
+            strArray={portfolioArray}
+            idx={15}
+            letterClass={letterClass}
+          />
+        </h1>
+      </div>
+    </>
+  );
+};
+
+export default Work;
