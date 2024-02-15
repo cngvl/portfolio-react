8ab37a141547a582c0caec2e28cf57cc6a587269
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-02-15 22:49:43 +1100

    cleaned up hardcoded animation interval values

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,12 @@
 import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
 import Loader from 'react-loaders';
-// import LogoTitle from '../../assets/images/logo-s.png';
-// import Logo from './Logo';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 
 const Home = () => {
   const [letterClass, setLetterClass] = useState('text-animate');
-  const introString = "I'm Viet";
-  const introArray = introString.split('');
-  // console.log();
+  const hiArray = 'Hi,'.split('');
+  const introArray = "I'm Viet".split('');
   const jobString = 'Software Engineer.';
   const jobArray = jobString.split('');
 
@@ -29,11 +25,12 @@ const Home = () => {
       <div className="container home-page">
         <div className="text-zone">
           <h1>
-            <span className={letterClass}>H</span>
-            <span className={`${letterClass} _12`}>i,</span>
+            <AnimatedLetters
+              letterClass={letterClass}
+              strArray={hiArray}
+              idx={12}
+            />
             <br />
-            {/* <span className={`${letterClass} _13`}>I</span>
-            <span className={`${letterClass} _14`}>'m </span> */}
             <AnimatedLetters
               letterClass={letterClass}
               strArray={introArray}
