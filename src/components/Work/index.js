14439fa8b021cd80e2b37103d7f4839f6278118d
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-02-19 23:54:11 +1100

    added basic work info

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -1,6 +1,7 @@
 import './index.scss';
 import AnimatedLetters from '../AnimatedLetters';
 import { useEffect, useState } from 'react';
+import Job from './Job';
 
 const Work = () => {
   const portfolioArray = 'Work'.split('');
@@ -18,8 +19,8 @@ const Work = () => {
 
   return (
     <>
-      <div className="container portfolio-page">
-        <div className="text-zone">
+      <div className="container work-page">
+        <div className="work-header">
           <h1>
             <AnimatedLetters
               strArray={portfolioArray}
@@ -27,6 +28,23 @@ const Work = () => {
               letterClass={letterClass}
             />
           </h1>
+        </div>
+        <div className="work-container">
+          <Job
+            jobTitle={'Barista'}
+            jobCompany={'The Usual Joint'}
+            jobDate={'Sept 2023 - Present'}
+          />
+          <Job
+            jobTitle={'Science Content Author'}
+            jobCompany={'Oxford University Press'}
+            jobDate={'Mar 2022 - Present'}
+          />
+          <Job
+            jobTitle={'Senior Tutor'}
+            jobCompany={'Nam Quang Tuition'}
+            jobDate={'Feb 2017 - Dec 2022'}
+          />
         </div>
       </div>
     </>
