ae393b1f4d46d293ea5f5d45a38f15794c998a73
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-02-19 23:53:53 +1100

    adjusted to clean up formatting - still needs more work

--- src/components/Portfolio/Project/index.js
+++ src/components/Portfolio/Project/index.js
@@ -15,12 +15,14 @@ const Project = ({
           alt="placeholder"
           className="project-preview"
         />
-        <h2>{ProjectTitle}</h2>
-        <p>Stack used: {ProjectStack}</p>
-        <p>{ProjectDescription}</p>
-        <a href={ProjectLink} target="_blank" rel="noreferrer">
-          Visit Project
-        </a>
+        <div className="project-text">
+          <h2>{ProjectTitle}</h2>
+          <p>Stack used: {ProjectStack}</p>
+          <p>{ProjectDescription}</p>
+          <a href={ProjectLink} target="_blank" rel="noreferrer">
+            Visit Project
+          </a>
+        </div>
       </div>
     </>
   );
