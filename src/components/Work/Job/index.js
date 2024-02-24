2371e6c0df2ceaa281f2bd3472883af8073e8387
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-02-24 23:45:50 +1100

    changed class name to maintain consistency throughout all files

--- src/components/Work/Job/index.js
+++ src/components/Work/Job/index.js
@@ -2,7 +2,7 @@ import './index.scss';
 
 const Job = ({ jobTitle, jobCompany, jobDate, jobDescription }) => {
   return (
-    <div className="jobBlock">
+    <div className="job-block">
       <h2>{jobTitle}</h2>
       <h3>{jobDate}</h3>
       <p>{jobCompany}</p>
