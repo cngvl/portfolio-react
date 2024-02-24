bff5a7ca5824259294ca1de738c57ce1dc6817a0
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-02-24 23:44:50 +1100

    added more info to description of  projects

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -40,26 +40,39 @@ const Portfolio = () => {
               'Designed an app to find available on-street parking in Melbourne CBD in real time. Using City of Melbourne’s database API, MapBox GL JS, Ruby on Rails and Google Maps to display the latest data.'
             }
             ProjectPreview={PlaceHolderImage}
+            ProjectLink={'https://melbourne-park-finder.herokuapp.com/'}
           />
           <Project
             ProjectTitle={'PhraseTracker Discord Bot'}
             ProjectPreview={PlaceHolderImage}
             ProjectStack={'Javascript, Node.js, Discord.js'}
+            ProjectDescription={
+              'This was a small project I wanted to make for my friends to use on our Discord channel. It works by calling the command with a phrase to look out for and a target user ( can also be randomised ). After the phrase has been mentioned a certain amount of times, the bot will generate a playful message.'
+            }
           />
           <Project
-            ProjectTitle={'React Portfolio'}
+            ProjectTitle={'Personal Portfolio'}
             ProjectPreview={PlaceHolderImage}
             ProjectStack={'HTML, CSS, React'}
+            ProjectDescription={
+              "This is the Portfolio you're looking at right now. What else is there to say here."
+            }
           />
           <Project
-            ProjectTitle={'WeatherScheduler'}
+            ProjectTitle={'Weather Scheduler'}
             ProjectPreview={PlaceHolderImage}
-            ProjectStack={('Python', 'Twilio')}
+            ProjectStack={'Python, Twilio'}
+            ProjectDescription={
+              'I drew inspiration for this project from a TikTik video by @tech_sis8. I thought that this was a good beginner-friendly project that I could eventually up-scale when I want to get into tools that require CRON scheduling and / or SMS messaging'
+            }
           />
           <Project
             ProjectTitle={'PDF Merger'}
             ProjectStack={'Python, Shell Scripting'}
             ProjectPreview={PlaceHolderImage}
+            ProjectDescription={
+              "A quick python script I made because I found myself often using online PDF editors which seemed super sketchy, especially when I was working with documents containing sensitive information. Works completely offline so I won't need to worry about my info being stolen. I plan to add additional features such as removing and reordering files soon!"
+            }
           />
         </div>
       </div>
