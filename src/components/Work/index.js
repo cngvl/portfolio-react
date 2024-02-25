ff7f6b8be6134cd3b15b3500bc02a5d70ebfccd8
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2024-02-25 21:55:30 +1100

    edited info and fixed p rule

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -35,16 +35,25 @@ const Work = () => {
             jobTitle={'Barista'}
             jobCompany={'The Usual Joint'}
             jobDate={'Sept 2023 - Present'}
+            jobDescription={
+              'I excel at spilling drinks and making silky smooth milk for coffee.'
+            }
           />
           <Job
             jobTitle={'Science Content Author'}
             jobCompany={'Oxford University Press'}
             jobDate={'Mar 2022 - Present'}
+            jobDescription={
+              'I read through VCE textbooks prior to production to ensure scientific accuracy and produce revision material.'
+            }
           />
           <Job
             jobTitle={'Senior Tutor'}
             jobCompany={'Nam Quang Tuition'}
             jobDate={'Feb 2017 - Dec 2022'}
+            jobDescription={
+              'My biggest priority for teaching students was making sure they FULLY understood the content by developing their critical thinking and logical reasoning, rather than trying to mindlessly solve stacks of questions'
+            }
           />
         </div>
       </div>
