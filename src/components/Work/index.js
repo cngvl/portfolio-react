ce20f8c0acedc16d49dc2a00a92d0a1e6c5ec424
Viet Le cuongle1998@hotmail.com <cuongle1998@hotmail.com>
2025-08-25 17:29:47 +1000

    tes

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -36,7 +36,7 @@ const Work = () => {
             jobCompany={'The Usual Joint'}
             jobDate={'Sept 2023 - Present'}
             jobDescription={
-              'I excel at spilling drinks and making silky smooth milk for coffee.'
+              'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
             }
           />
           <Job
